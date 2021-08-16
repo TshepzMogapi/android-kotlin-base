699ad3b2b8bb0ef617b797dfff7b7959f23d5d6e
Tshepiso Mogapi <tshepzmogapi@gmail.com>
2021-08-16 23:55:41 +0200

    add dice images and update img onClick

--- app/src/main/java/com/example/android_kotlin_base/MainActivity.kt
+++ app/src/main/java/com/example/android_kotlin_base/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.android_kotlin_base
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
@@ -15,9 +16,17 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun randomize() {
-        val randomText: TextView = findViewById(R.id.random_txt)
         val randomInt = (1..6).random()
-        randomText.setText(randomInt.toString())
+        val diceIamge: ImageView = findViewById(R.id.dice_img)
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceIamge.setImageResource(drawableResource)
         Toast.makeText(this, "Random thing!!!",
         Toast.LENGTH_SHORT).show()
     }
