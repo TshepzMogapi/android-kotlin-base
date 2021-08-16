00c1060f2794aaa4d643b2ad0a78a82171f93c12
Tshepiso Mogapi <tshepzmogapi@gmail.com>
2021-08-16 23:31:08 +0200

    anatomy of basic android

--- app/src/main/java/com/example/android_kotlin_base/MainActivity.kt
+++ app/src/main/java/com/example/android_kotlin_base/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.android_kotlin_base
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val randomizeBtn: Button = findViewById(R.id.randomize_btn)
+        randomizeBtn.setOnClickListener{ randomize() }
+    }
+
+    private fun randomize() {
+        val randomText: TextView = findViewById(R.id.random_txt)
+        val randomInt = (1..6).random()
+        randomText.setText(randomInt.toString())
+        Toast.makeText(this, "Random thing!!!",
+        Toast.LENGTH_SHORT).show()
     }
 }
