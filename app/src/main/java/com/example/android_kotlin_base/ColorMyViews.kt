e745f9fae081296dae5ad03655e0f7a82ad4bc4b
Tshepiso Mogapi <tshepzmogapi@gmail.com>
2021-09-08 00:19:39 +0200

    more baseline stuff

--- app/src/main/java/com/example/android_kotlin_base/ColorMyViews.kt
+++ app/src/main/java/com/example/android_kotlin_base/ColorMyViews.kt
@@ -4,6 +4,7 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
 
 class ColorMyViews : AppCompatActivity() {
@@ -22,6 +23,10 @@ class ColorMyViews : AppCompatActivity() {
             R.id.box_three_text -> view.setBackgroundColor(Color.BLUE)
             R.id.box_four_text -> view.setBackgroundColor(Color.MAGENTA)
             R.id.box_five_text -> view.setBackgroundColor(Color.BLUE)
+
+            R.id.red_button -> view.setBackgroundResource(R.color.my_red)
+            R.id.yellow_button -> view.setBackgroundResource(R.color.my_yellow)
+            R.id.green_button -> view.setBackgroundResource(R.color.my_green)
             else -> view.setBackgroundColor(Color.LTGRAY)
         }
     }
@@ -34,11 +39,18 @@ class ColorMyViews : AppCompatActivity() {
         val boxFourText = findViewById<TextView>(R.id.box_four_text)
         val boxFiveText = findViewById<TextView>(R.id.box_five_text)
 
+        val redButton = findViewById<Button>(R.id.red_button)
+        val greenButton = findViewById<Button>(R.id.green_button)
+        val yellowButton = findViewById<Button>(R.id.yellow_button)
+
+
         val rootConstraintLayout = findViewById<View>(R.id.constraint_layout)
 
         val clickableViews: List<View> =
                 listOf(boxOneText, boxTwoText, boxThreeText,
-                        boxFourText, boxFiveText, rootConstraintLayout)
+                        boxFourText, boxFiveText, rootConstraintLayout,
+                        redButton, greenButton, yellowButton
+                )
 
         for (item in clickableViews) {
             item.setOnClickListener { colorize(it) }
