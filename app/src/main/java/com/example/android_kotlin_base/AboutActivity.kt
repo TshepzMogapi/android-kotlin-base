b0209016d26caf11eccd76a84cc4d5f735f69f49
Tshepiso Mogapi <tshepzmogapi@gmail.com>
2021-09-10 00:55:09 +0200

    use data binding

--- app/src/main/java/com/example/android_kotlin_base/AboutActivity.kt
+++ app/src/main/java/com/example/android_kotlin_base/AboutActivity.kt
@@ -4,21 +4,24 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.example.android_kotlin_base.databinding.ActivityAboutBinding
 
 class AboutActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityAboutBinding
+    private val info: Info = Info("1991")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_about)
-        val submitButton: Button = findViewById(R.id.submit_answer_btn)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_about)
+        binding.info = info
+        val submitButton: Button = binding.submitAnswerBtn
         submitButton.setOnClickListener { showInfo() }
     }
 
     private fun showInfo() {
-        val kotlinInfoTextView = findViewById<TextView>(R.id.about_android_kotlin_tv)
-        val answerEditText = findViewById<EditText>(R.id.random_answer_et)
+        val kotlinInfoTextView = binding.aboutAndroidKotlinTv
+        val answerEditText = binding.randomAnswerEt
         var infoMessage = "InCorrect"
         if (answerEditText.text.toString() == ("2019")) {
             infoMessage = "Correct"
---
+++ app/src/main/java/com/example/android_kotlin_base/Info.kt
@@ -0,0 +1,3 @@
+package com.example.android_kotlin_base
+
+data class Info(var year: String = "")
